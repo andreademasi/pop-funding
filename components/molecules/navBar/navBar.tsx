206ac86889d675b197446cf3fa837a2a1144c254
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-05-28 17:40:08 +0200

    Fixed env

--- components/molecules/navBar/navBar.tsx
+++ components/molecules/navBar/navBar.tsx
@@ -20,10 +20,11 @@ const NavBar = () => {
   return (
     <nav className="absolute top-0 my-5 flex w-full flex-row justify-evenly align-middle  ">
       {items.map((item, index) => (
-        <span className="after:content-[' '] z-10 flex flex-col text-smallA after:h-px after:w-0 after:self-center after:bg-brown after:transition-all after:duration-300 after:hover:w-full md:text-bigA">
-          <Link key={index} href={item.link}>
-            {item.name}
-          </Link>
+        <span
+          key={index}
+          className="after:content-[' '] z-10 flex flex-col text-smallA after:h-px after:w-0 after:self-center after:bg-brown after:transition-all after:duration-300 after:hover:w-full md:text-bigA"
+        >
+          <Link href={item.link}>{item.name}</Link>
         </span>
       ))}
     </nav>
