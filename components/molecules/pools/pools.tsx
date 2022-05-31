c392c35ff1a43aa416f174327626a0c4c695b615
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-05-31 23:59:05 +0200

    Added things

--- components/molecules/pools/pools.tsx
+++ components/molecules/pools/pools.tsx
@@ -5,30 +5,37 @@ import { ItemPool } from '../fundingsHero/fundingsHero'
 
 interface PoolsProps {
   poolsArray: Array<ItemPool>
+  type: string
 }
 
-const Pools = ({ poolsArray }: PoolsProps) => {
+const Pools = ({ poolsArray, type }: PoolsProps) => {
   return (
     <div className="mx-4 flex flex-col justify-center align-middle">
       <div className="flex w-full flex-row flex-wrap justify-center align-middle">
-        {poolsArray
-          .sort((a, b) => {
-            return a.dateStart - b.dateStart
-          })
-          .map((item, index) => {
-            return (
-              <Pool
-                title={item.title}
-                description={item.description}
-                dateStart={item.dateStart}
-                dateEnd={item.dateEnd}
-                dateClose={item.dateClose}
-                goal={item.goal}
-                current={item.current}
-                key={index}
-              />
-            )
-          })}
+        {poolsArray.length > 0 ? (
+          poolsArray
+            .sort((a, b) => {
+              return a.dateStart - b.dateStart
+            })
+            .map((item, index) => {
+              return (
+                <Pool
+                  title={item.title}
+                  description={item.description}
+                  dateStart={item.dateStart}
+                  dateEnd={item.dateEnd}
+                  dateClose={item.dateClose}
+                  goal={item.goal}
+                  current={item.current}
+                  key={index}
+                />
+              )
+            })
+        ) : (
+          <p className="mx-auto mb-16 w-fit rounded-2xl border-2 border-brown px-8 py-4 text-center text-lg shadow-2xl">
+            There are no {type} pools
+          </p>
+        )}
       </div>
     </div>
   )
