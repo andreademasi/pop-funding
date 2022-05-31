337378c61c20b0cb62c8f7327d05a98734004a74
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-05-31 10:58:02 +0200

    Added datetime pickers

--- components/atoms/pool/pool.tsx
+++ components/atoms/pool/pool.tsx
@@ -9,15 +9,18 @@ interface PoolProps {
 
 const dateConverter = (date: number) => {
   const x = new Date(date)
-  const y = x.toLocaleDateString()
-  return y
+  const y = x.toString().split('GMT')[0].slice(4)
+  return y == 'lid Date' ? x.toString() : y
 }
 
 const Pool = ({ title, description, dateStart, dateEnd }: PoolProps) => {
   return (
     <div className="m-4 flex h-fit flex-[1_1_280px] flex-col justify-center rounded-2xl bg-opacity-10 bg-gradient-to-tl from-transparentPurple via-transparentDarkBlue to-transparentPink align-middle shadow-lg transition-transform hover:scale-[1.01]">
       <p className="relative top-2 left-2 w-fit rounded-xl bg-transparentWhite px-2 py-1 text-sm opacity-70">
-        From: {dateConverter(dateStart)} - To: {dateConverter(dateEnd)}
+        From: {dateConverter(dateStart)}
+      </p>
+      <p className="relative top-2 left-2 w-fit rounded-xl bg-transparentWhite px-2 py-1 text-sm opacity-70">
+        To: {dateConverter(dateEnd)}
       </p>
       <h3 className="my-4 mx-4 text-xl font-bold tracking-wider">{title}</h3>
       <p className="mx-6 mt-4 mb-8 max-h-[10em] overflow-y-scroll whitespace-pre-wrap px-2 font-thin opacity-90">
