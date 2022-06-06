b6e1d9f7c201d17e1f81863f0ff24d20e7c5919a
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-06-06 10:42:07 +0200

    Added stupid things

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
     webpack: (config, options) => {
         config.module.rules.push({
             test: /\.teal/,
-            use: 'raw-loader'
+            use: 'raw-loader',
         })
 
         return config
