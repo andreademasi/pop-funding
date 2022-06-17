74d65bd00dce396ad2277b7feadddab6a78197be
Andrea De Masi <andreademasi80@gmail.com>
2022-06-17 18:04:47 +0200

    Removed unused code

--- helpers/api.ts
+++ helpers/api.ts
@@ -401,39 +401,3 @@ export async function apiGetAccountAssets(
 
   return assets
 }
-
-export async function apiGetTxnParams(
-  chain: ChainType
-): Promise<algosdk.SuggestedParams> {
-  const params = await clientForChain(chain).getTransactionParams().do()
-  return params
-}
-
-export async function apiSubmitTransactions(
-  chain: ChainType,
-  stxns: Uint8Array[]
-): Promise<number> {
-  const { txId } = await clientForChain(chain).sendRawTransaction(stxns).do()
-  return await waitForTransaction(chain, txId)
-}
-
-async function waitForTransaction(
-  chain: ChainType,
-  txId: string
-): Promise<number> {
-  const client = clientForChain(chain)
-
-  let lastStatus = await client.status().do()
-  let lastRound = lastStatus['last-round']
-  while (true) {
-    const status = await client.pendingTransactionInformation(txId).do()
-    if (status['pool-error']) {
-      throw new Error(`Transaction Pool Error: ${status['pool-error']}`)
-    }
-    if (status['confirmed-round']) {
-      return status['confirmed-round']
-    }
-    lastStatus = await client.statusAfterBlock(lastRound + 1).do()
-    lastRound = lastStatus['last-round']
-  }
-}
